364bbf2607c383a39e16eab79dc83e4934a49f10
Stas Skokov <7090stas@gmail.com>
2025-01-20 17:53:36 +1100

    fix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ extra["targetSdkVersion"] = 35
 extra["versionMajor"] = 1
 extra["versionMinor"] = 0
 extra["versionPatch"] = 0
-extra["versionBuild"] = 1
+extra["versionBuild"] = 5
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
