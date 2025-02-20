866f8c251b017f2ac5f24e770cd30f0a6181e362
Stas Skokov <7090stas@gmail.com>
2025-02-20 19:15:24 +1100

    fix constants

---
+++ app/src/main/kotlin/org/fptn/vpnclient/core/common/Constants.kt
@@ -0,0 +1,9 @@
+package org.fptn.vpnclient.core.common
+
+object Constants {
+    const val MAIN_NOTIFICATION_CHANNEL_ID = "fptnvpn-notification-main"
+    const val MAIN_NOTIFICATION_CHANNEL_GROUP_ID = "fptnvpn-notification-main-group"
+    const val SELECTED_SERVER: String = "fptn.selected.server"
+    const val SELECTED_SERVER_ID_AUTO: Int = -1
+    const val MAIN_CONNECTED_NOTIFICATION_ID = 8975
+}
