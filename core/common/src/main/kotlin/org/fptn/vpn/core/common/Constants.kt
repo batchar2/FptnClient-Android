ae5dd79120d2ac1abae2e132290b6dae2deb7be7
Fil <enin.ss.nsk@gmail.com>
2025-03-05 13:36:23 +0700

    disable notification sound

--- core/common/src/main/kotlin/org/fptn/vpn/core/common/Constants.kt
+++ core/common/src/main/kotlin/org/fptn/vpn/core/common/Constants.kt
@@ -3,6 +3,8 @@ package org.fptn.vpn.core.common
 object Constants {
     const val APPLICATION_SHARED_PREFERENCES = "fptnvpn-shared-preferences"
     const val MAIN_NOTIFICATION_CHANNEL_ID = "fptnvpn-notification-main"
+    const val MAIN_NOTIFICATION_CHANNEL_VERSION = "fptnvpn-notification-main-channel-version"
+    const val MAIN_NOTIFICATION_CHANNEL_VERSION_NUM = 2
     const val MAIN_NOTIFICATION_CHANNEL_GROUP_ID = "fptnvpn-notification-main-group"
     const val SELECTED_SERVER: String = "fptn.selected.server"
     const val SELECTED_SERVER_ID_AUTO: Int = -1
