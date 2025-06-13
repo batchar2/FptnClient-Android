dc88fa8058e00956de38532895a6f3dc6af4c2e5
Stas Skokov <7090stas@gmail.com>
2025-06-13 22:46:22 +1000

    fix leak

--- app/src/main/cpp/src/websocket_client.cpp
+++ app/src/main/cpp/src/websocket_client.cpp
@@ -6,11 +6,56 @@ Distributed under the MIT License (https://opensource.org/licenses/MIT)
 =============================================================================*/
 
 #include <jni.h>
+#include <mutex>
+#include <unordered_map>
 
 #include "wrappers/utils/utils.h"
 #include "wrappers/wrapper_websocket_client/wrapper_websocket_client.h"
 
 using fptn::wrapper::WrapperWebsocketClient;
+
+class SafeProxy {
+ private:
+  static std::mutex mutex_;
+  static std::unordered_map<jlong, bool> status_clients_;
+
+ public:
+  SafeProxy() {
+    mutex_.lock();  // Lock on construction
+  }
+
+  ~SafeProxy() {
+    mutex_.unlock();  // Unlock on destruction (RAII)
+  }
+
+  void Add(jlong client) { status_clients_[client] = true; }
+
+  void Delete(jlong client) {
+    auto it = status_clients_.find(client);
+    if (it == status_clients_.end()) {
+      status_clients_.erase(it);
+    }
+  }
+
+  WrapperWebsocketClient* Get(jlong client) {
+    auto it = status_clients_.find(client);
+    if (it != status_clients_.end() && it->second) {
+      return reinterpret_cast<WrapperWebsocketClient*>(client);
+    }
+    return nullptr;
+  }
+
+  // Disable copying
+  SafeProxy(const SafeProxy&) = delete;
+  SafeProxy& operator=(const SafeProxy&) = delete;
+
+  // Optionally, you might want to add move operations
+  SafeProxy(SafeProxy&&) = delete;
+  SafeProxy& operator=(SafeProxy&&) = delete;
+};
+
+std::mutex SafeProxy::mutex_;
+std::unordered_map<jlong, bool> SafeProxy::status_clients_;
 
 // create an object
 extern "C" JNIEXPORT jlong JNICALL
@@ -23,6 +68,8 @@ Java_org_fptn_vpn_services_websocket_NativeWebSocketClientImpl_nativeCreate(
     jstring sni_param,
     jstring access_token_param,
     jstring expected_md5_fingerprint_param) {
+  fptn::wrapper::init_logger();  // will call only once
+
   auto server_ip = fptn::wrapper::ConvertToCString(env, server_ip_param);
   int server_port = server_port_param;
   auto tun_ipv4 = fptn::wrapper::ConvertToCString(env, tun_ipv4_param);
@@ -35,7 +82,13 @@ Java_org_fptn_vpn_services_websocket_NativeWebSocketClientImpl_nativeCreate(
   auto* websocket_client = new WrapperWebsocketClient(global_object_ref,
       std::move(server_ip), server_port, std::move(tun_ipv4), std::move(sni),
       std::move(access_token), std::move(expected_md5_fingerprint));
-  return reinterpret_cast<jlong>(websocket_client);
+
+  auto jobj_client = reinterpret_cast<jlong>(websocket_client);
+
+  SafeProxy proxy;
+  proxy.Add(jobj_client);
+
+  return jobj_client;
 }
 
 // Destroy
@@ -45,11 +98,14 @@ Java_org_fptn_vpn_services_websocket_NativeWebSocketClientImpl_nativeDestroy(
   (void)env;
   (void)thiz;
 
-  auto* websocket_client =
-      reinterpret_cast<WrapperWebsocketClient*>(native_handle);
+  SafeProxy proxy;
+
+  auto* websocket_client = proxy.Get(native_handle);
   if (websocket_client) {
     websocket_client->Stop();
     delete websocket_client;
+
+    proxy.Delete(native_handle);
   }
 }
 
@@ -61,8 +117,9 @@ Java_org_fptn_vpn_services_websocket_NativeWebSocketClientImpl_nativeRun(
   (void)thiz;
 
   bool status = false;
-  auto* websocket_client =
-      reinterpret_cast<WrapperWebsocketClient*>(native_handle);
+
+  SafeProxy proxy;
+  auto* websocket_client = proxy.Get(native_handle);
   if (websocket_client) {
     status = websocket_client->Start();
   }
@@ -76,11 +133,11 @@ Java_org_fptn_vpn_services_websocket_NativeWebSocketClientImpl_nativeStop(
   (void)env;
   (void)thiz;
 
-  fptn::wrapper::init_logger();  // will call only once
-
   bool status = false;
-  auto* websocket_client =
-      reinterpret_cast<WrapperWebsocketClient*>(native_handle);
+
+  SafeProxy proxy;
+
+  auto* websocket_client = proxy.Get(native_handle);
   if (websocket_client) {
     SPDLOG_INFO("Stop websocket");
     status = websocket_client->Stop();
@@ -95,9 +152,10 @@ Java_org_fptn_vpn_services_websocket_NativeWebSocketClientImpl_nativeSend(
   (void)thiz;
 
   bool status = false;
-  auto* websocket_client =
-      reinterpret_cast<WrapperWebsocketClient*>(native_handle);
-  if (websocket_client && env) {
+
+  SafeProxy proxy;
+  auto* websocket_client = proxy.Get(native_handle);
+  if (websocket_client && env && data) {
     // Java bytes to std::string
     jbyte* buffer = env->GetByteArrayElements(data, nullptr);
     const jsize length = env->GetArrayLength(data);
@@ -110,6 +168,7 @@ Java_org_fptn_vpn_services_websocket_NativeWebSocketClientImpl_nativeSend(
       env->ReleaseByteArrayElements(data, buffer, JNI_ABORT);
     }
   }
+
   return static_cast<jboolean>(status);
 }
 
@@ -121,10 +180,11 @@ Java_org_fptn_vpn_services_websocket_NativeWebSocketClientImpl_nativeIsStarted(
   (void)thiz;
 
   bool status = false;
-  auto* websocket_client =
-      reinterpret_cast<WrapperWebsocketClient*>(native_handle);
+  SafeProxy proxy;
+  auto* websocket_client = proxy.Get(native_handle);
   if (websocket_client) {
     status = websocket_client->IsStarted();
   }
+
   return static_cast<jboolean>(status);
 }
