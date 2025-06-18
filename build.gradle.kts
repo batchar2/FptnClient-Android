709c18950effeef092fa464fc022e3b17e002cb0
Stas Skokov <7090stas@gmail.com>
2025-06-18 20:16:08 +1000

    [bugfix] prevent crash in bssl::ssl_handle_open_record during SSL_read

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ extra["minSdkVersion"] = 28
 extra["targetSdkVersion"] = 35
 extra["versionMajor"] = 1
 extra["versionMinor"] = 2
-extra["versionPatch"] = 5
-extra["versionBuild"] = 0
+extra["versionPatch"] = 6
+extra["versionBuild"] = 1
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
