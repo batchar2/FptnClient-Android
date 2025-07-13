f3b4e9d1e69dba09eb712f363d95177e8055e3a0
Stas Skokov <7090stas@gmail.com>
2025-07-13 19:12:53 +1000

    fix debug build

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,7 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
+        classpath(libs.google.services)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
