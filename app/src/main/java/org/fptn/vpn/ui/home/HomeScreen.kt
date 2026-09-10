579a4fabe16ad9eaf51216c316259efb5897d4a6
brightsunshine54 <brightsunshine54@gmail.com>
2026-09-11 02:13:39 +0700

    fix ping

--- app/src/main/java/org/fptn/vpn/ui/home/HomeScreen.kt
+++ app/src/main/java/org/fptn/vpn/ui/home/HomeScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -484,8 +485,8 @@ fun HomeScreen(
                             enabled = !activeState,
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .height(48.dp)
-                                .padding(top = 8.dp),
+                                .heightIn(min = 36.dp)
+                                .padding(top = 16.dp),
                         )
                     }
 
